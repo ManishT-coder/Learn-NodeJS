2282904dcc148111e032318f8d00dfe0a7a77fbe
ManishT-coder <manishtigaya6148@gmail.com>
2026-06-02 16:55:36 +0530

    Created a Server with routes

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const http = require('http');
+const fs = require('fs');
 
 const Myserver = http.createServer((req, res) => {
-    console.log('Request received');
-    res.end('Hello, World!');
+   
+    // to print the headers of the request
+    //console.log(req.headers);
+
+    //to print entire request object
+    // console.log(req);
+    // res.end('Hello, World!');
+
+    const log = `${Date.now()}:${req.url} New request received\n`;
+    fs.appendFile('request.log', log, (err ,data) => {
+        switch(req.url){
+            case '/':
+                res.end('Home Page');
+                break;
+            case '/about':
+                res.end('Hii I am Manish');
+                break;
+            default:
+                res.end('Page Not Found');
+        }
+        
+        
+
+});
 });
 
-Myserver.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+Myserver.listen(8000, () => {
+    console.log('Server is listening on port 8000');
 }); 
-
-console.log("Testing Git commit");
