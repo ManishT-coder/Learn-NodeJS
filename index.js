1d551d717f682320a59e79c389f14806e47b0a97
ManishT-coder <manishtigaya6148@gmail.com>
2026-06-02 15:18:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const Myserver = http.createServer((req, res) => {
+    console.log('Request received');
+    res.end('Hello, World!');
+});
+
+Myserver.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+}); 
