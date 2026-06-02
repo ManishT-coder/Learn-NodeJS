feca04bc245ff74b87d420048815811943c93613
ManishT-coder <manishtigaya6148@gmail.com>
2026-06-02 14:59:15 +0530

    check how many cpus in the system

--- file.js
+++ file.js
@@ -1,36 +1,43 @@
 const fs = require('fs');
 
+const os = require('os');
+
+console.log(os.cpus().length);
+
 // synchronous
 // writeFileSync is used to write data to a file synchronously. 
 // It takes two arguments: the name of the file and the data to be written.
 //  If the file does not exist, it will be created.
 //  If the file already exists, it will be overwritten.
 // synchronous
-fs.writeFileSync('text.txt','Hello World');
+
+//fs.writeFileSync('text.txt','Hello World');
 
 // Asynchronous
 // writeFile is used to write data to a file asynchronously.
 // It takes three arguments: the name of the file, the data to be written, and a callback function that is called when the write operation is complete.
 // If the file does not exist, it will be created.
 // If the file already exists, it will be overwritten.
-fs.writeFile('text.txt','Hello World Asynchronous', (err) => {});
+
+//fs.writeFile('text.txt','Hello World Asynchronous', (err) => {});
 
 // By using readFileSync
 // readFileSync is used to read data from a file synchronously.
-const names = fs.readFileSync('names.txt','utf-8');
-console.log(names);
+
+//const names = fs.readFileSync('names.txt','utf-8');
+//console.log(names);
 
 
 // by using readFile
-fs.readFile('names.txt','utf-8',(err,data) => {
-    if(err) {
-        console.log(err);}
-    else {
-        console.log(data);
-    }
-});
+//fs.readFile('names.txt','utf-8',(err,data) => {
+//    if(err) {
+//        console.log(err);}
+//    else {
+//        console.log(data);
+//    }
+// });
 
-fs.unlinkSync('names.txt'); 
+//fs.unlinkSync('names.txt'); 
 // to delete a file synchronously
 // fs.unlink('names.txt',(err) => { // to delete a file asynchronously
 //     if(err) {    
