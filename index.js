3c9fbb933d4e40194999df3f5d06e0e1a9a591cb
ManishT-coder <manishtigaya6148@gmail.com>
2026-06-03 12:24:13 +0530

    Routing by using Express

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
 const http = require('http');
-const fs = require('fs');
-const url = require('url');
+// const fs = require('fs');
+// const url = require('url');
 
-const Myserver = http.createServer((req, res) => {
-           if(req.url === "/favicon.ico")
-            return res.end();
-    // to print the headers of the request
-    //console.log(req.headers);
+// Express is a popular web framework for Node.js that simplifies the process of building web applications and APIs.
+//  It provides a robust set of features for handling routing, middleware,
+//  and HTTP requests and responses,
+//  making it easier to create server-side applications.
+// without using url all are manged by express
+const express = require('express');
+const app = express();  
 
-    //to print entire request object
-    // console.log(req);
-    // res.end('Hello, World!');
+app.get('/', (req, res) => {
+    return res.send('Home Page');
+});
 
-    // Route handling
-    const log = `${Date.now()}:${req.url} New request received\n`;
-    fs.appendFile('request.log', log, (err ,data) => {
-        const myUrl = url.parse(req.url, true);
-        console.log(myUrl);
-        switch(myUrl.pathname){
-            case '/':
-                res.end('Home Page');
-                break;
-            case '/about':
-                const username = myUrl.query.name;
-                res.end(`Hello, ${username}!`);
-                break;
-            default:
-                res.end('Page Not Found');
-        }
+app.get('/about', (req, res) => {
+    return res.send('About Page');
+});
+const Myserver = http.createServer(app);
+
+// const Myserver = http.createServer((req, res) => {
+//            if(req.url === "/favicon.ico")
+//             return res.end();
+//     // to print the headers of the request
+//     //console.log(req.headers);
+
+//     //to print entire request object
+//     // console.log(req);
+//     // res.end('Hello, World!');
+
+//     // Route handling
+//     const log = `${Date.now()}:${req.url} New request received\n`;
+//     fs.appendFile('request.log', log, (err ,data) => {
+//         const myUrl = url.parse(req.url, true);
+//         console.log(myUrl);
+//         switch(myUrl.pathname){
+//             case '/':
+//                 res.end('Home Page');
+//                 break;
+//             case '/about':
+//                 const username = myUrl.query.name;
+//                 res.end(`Hello, ${username}!`);
+//                 break;
+//             default:
+//                 res.end('Page Not Found');
+//         }
         
         
 
-});
-});
+// });
+// });
 
 Myserver.listen(8000, () => {
     console.log('Server is listening on port 8000');
